d34cc75f55e07588f334fda38229a96524344507
Pita Badila <ritualmomma@gmail.com>
2023-05-04 15:21:28 -0400

    Takes out password in config.js

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-const {Recipe} = require("../models");
+const { Recipe } = require("../models");
+//GET all recipes
+// router.get('/', async(req,res) => {
+//     try{
+//         const recipes = await Recipe.findAll()
+//         res.json(recipes)
+//     }
+//     catch (error){
+//         res.status(500).json({ message: "Error retrieving random recipe", error });
+//     }
+// })
+
 
 //GET randomized recipe
 router.get('/random',async(req,res) => {
     try{
+        console.log('trying')
         const count = await Recipe.count()
 
         //function to generate a random number between 1 and the number of rows of the Recipe table
