279fd602bc7bcadfb9421fbacbd3426243f4e803
Pita Badila <ritualmomma@gmail.com>
2023-05-05 13:47:43 -0400

    this makes the route for searching recipes by ingredient

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,35 +2,47 @@ var express = require('express');
 var router = express.Router();
 
 const { Recipe } = require("../models");
-//GET all recipes
-// router.get('/', async(req,res) => {
-//     try{
-//         const recipes = await Recipe.findAll()
-//         res.json(recipes)
-//     }
-//     catch (error){
-//         res.status(500).json({ message: "Error retrieving random recipe", error });
-//     }
-// })
+const { Op } = require('sequelize');
 
-
-//GET randomized recipe
-router.get('/random',async(req,res) => {
-    try{
+router.get('/random', async (req, res) => {
+    try {
         console.log('trying')
         const count = await Recipe.count()
 
         //function to generate a random number between 1 and the number of rows of the Recipe table
-         let randomNum = () =>{
-            let num = 1+Math.floor(Math.random()*(count))
+        let randomNum = () => {
+            let num = 1 + Math.floor(Math.random() * (count))
             return num
         }
-       
-    const recipe = await Recipe.findByPk(randomNum())
-    res.json(recipe)
+
+        const recipe = await Recipe.findByPk(randomNum())
+        res.json(recipe)
     }
-    
-    catch (error){
+
+    catch (error) {
+        res.status(500).json({ message: "Error retrieving random recipe", error: message });
+    }
+})
+
+//GET recipes based on particular ingredient
+router.get('/:item', async (req, res) => {
+
+    try {
+        console.log('trying to show all with an item')
+        console.log(req.params.item)
+
+        const recipes = await Recipe.findAll({
+            where: {
+                ingredients: {
+                    [Op.like]: `%${req.params.item}%`
+                }
+            }
+        })
+        console.log(recipes)
+        res.json(recipes)
+    }
+
+    catch (error) {
         res.status(500).json({ message: "Error retrieving random recipe", error });
     }
 })
