db6294550a13801e07c1cc25afd1090c82a6bce2
COR Coding <corcoding@CORs-MacBook-12.localdomain>
2023-05-09 15:52:56 -0400

    modifies file path

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var recipesRouter = require('./routes/recipes');
 var factsRouter = require('./routes/facts');
-var checklistRouter = require('./routes/checklist');
+var checklistRouter = require('./routes/checkList');
 
 
 var app = express();
